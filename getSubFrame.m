0c28fad59ec64a5accaad11ab341a8c19c4addfd
rwarren2163 <richardwarren2163@gmail.com>
2018-01-17 14:27:47 -0500

    wip

--- getSubFrame.m
+++ getSubFrame.m
@@ -15,19 +15,18 @@ yInds = y1:y1+subFrameSize(1)-1;
 xInds = x1:x1+subFrameSize(1)-1;
 
 
-
+try
 % if image falls outside of frame, pad with zeros and then get subframe
 if any(yInds<=0) || any(yInds>size(frame,1)) || any(xInds<=0) || any(xInds>size(frame,2))
     
     frame = [zeros(subFrameSize(1), size(frame,2)); frame; zeros(subFrameSize(1), size(frame,2))]; % pad top and bottom
     frame = [zeros(size(frame,1), subFrameSize(2)), frame, zeros(size(frame,1), subFrameSize(2))]; % pad sides
-    try; subFrame = frame(yInds+subFrameSize(1), xInds+subFrameSize(2)); catch; keyboard; end
+    subFrame = frame(yInds+subFrameSize(1), xInds+subFrameSize(2));
     isPadded = true;
     
 % otherwise just take the friggin subframe
 else
-    
-    try; subFrame = frame(yInds, xInds); catch; keyboard; end
+    subFrame = frame(yInds, xInds);
     isPadded = false;
-    
 end
+catch; keyboard; end
