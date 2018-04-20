ce511714804860d1bee1c3be01a9b8537882cad7
rwarren2163 <richardwarren2163@gmail.com>
2018-04-19 20:06:16 -0400

    wip

--- tracking/deepLabCut/labelFrames.m
+++ tracking/deepLabCut/labelFrames.m
@@ -1,9 +1,15 @@
 function labelFrames(trainingSetName, features)
 
+% store locations in struct // restrict point locations // ensure a point has been moved before saving it's value
+
 % settings
 vidScaling = 2;
+textOffset = [5 0];
+nanColor = [1 0 0];
+labelledColor = [1 1 0];
 
 % initializations
+defaultPositions = repmat([10 10], length(features), 1) .* repmat(1:length(features),2,1)';
 load([getenv('TRAININGEXAMPLESDIR') 'deepLabCut\' trainingSetName '\trainingData.mat'], 'trainingData');
 fields = fieldnames(trainingData);
 structInd = 1;
@@ -11,7 +17,6 @@ structInd = 1;
 
 
 % create figure
-close all
 currentSession = trainingData(structInd).session;
 currentFrame = trainingData(structInd).frameNum;
 vid = VideoReader([getenv('OBSDATADIR') 'sessions\' currentSession '\runBot.mp4']);
@@ -19,12 +24,13 @@ fig = figure('name', sprintf('%s, frame %i', currentSession, currentFrame), ...
     'menubar', 'none', 'color', 'white', 'keypressfcn', @keypress, 'position', [200 200 vid.Width*vidScaling, vid.Height*vidScaling]);
 colormap gray
 im = image(rgb2gray(read(vid, currentFrame)), 'CDataMapping', 'scaled');
-set(gca, 'position', [0 0 1 1])
+set(gca, 'position', [0 0 1 1], 'visible', 'off')
 
 
 
 % add fields that have not yet been created, and creatable draggable objects
 featurePoints = cell(1, length(features));
+featureTexts = cell(1, length(features));
 
 for i = 1:length(features)
     
@@ -36,8 +42,11 @@ for i = 1:length(features)
     end
     
     % create draggables for features
-    featurePoints{i} = impoint(gca, [10 10]*i);
+    featureTexts{i} = text(defaultPositions(i,1)+textOffset(1), defaultPositions(i,2)+textOffset(2), features{i});
+    featurePoints{i} = impoint(gca, defaultPositions(i,:));
+    addNewPositionCallback(featurePoints{i}, @(x) movePoint(i));
 end
+updateFrame(0); % set positions and colors of points based on whether the first frame has already been labeled
 
 
 % ---------
@@ -62,12 +71,12 @@ function keypress(~,~)
         
 
             % ESCAPE: close window
-            case 27% save current progress
+            case 27
                 close(fig)
                 
             case 115 % 's'
                 save([getenv('TRAININGEXAMPLESDIR') 'deepLabCut\' trainingSetName '\trainingData.mat'], 'trainingData');
-                disp('data saved')
+                m = msgbox('saving progress...'); pause(.5); close(m)
         end
     end
 end
@@ -75,6 +84,7 @@ end
 
 function updateFrame(frameStep)
     
+    % move to next frame
     structInd = structInd + frameStep;
     currentFrame = trainingData(structInd).frameNum;
     
@@ -88,8 +98,29 @@ function updateFrame(frameStep)
     frame = rgb2gray(read(vid, trainingData(structInd).frameNum));
     set(im, 'CData', frame);
     set(fig, 'name', sprintf('%s, frame %i', currentSession, currentFrame))
+    
+    % update colors and positions of points
+    for j = 1:length(features)
+        pos = trainingData(structInd).(features{j});
+        
+        if any(isnan(pos)) % not yet tracked
+            set(featureTexts{j}, 'color', nanColor);
+            setColor(featurePoints{j}, nanColor);
+        else
+            set(featureTexts{j}, 'position', pos, 'color', labelledColor);
+            setColor(featurePoints{j}, labelledColor);
+            setPosition(featurePoints{j}, pos);
+        end
+    end
 end
 
+
+function movePoint(featureInd)
+    pos = getPosition(featurePoints{featureInd});
+    trainingData(structInd).(features{featureInd}) = pos;
+    setColor(featurePoints{featureInd}, labelledColor);
+    set(featureTexts{featureInd}, 'position', pos + textOffset, 'color', labelledColor);
+end
 
 end
 
