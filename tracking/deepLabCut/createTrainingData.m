569fed6e1d9abe528cf91bc5b5d77b757c9fef23
rwarren2163 <richardwarren2163@gmail.com>
2018-04-23 19:20:54 -0400

    wip

--- tracking/deepLabCut/createTrainingData.m
+++ tracking/deepLabCut/createTrainingData.m
@@ -1,7 +1,8 @@
 %% initialize new training data structure
 
 % settings
-trainingSetName = 'newTrainingData';
+trainingSetName = 'topBotCat';
+view = 'both';
 sessions = {'180122_001', '180122_002', '180122_003', ...
             '180123_001', '180123_002', '180123_003', ...
             '180124_001', '180124_002', '180124_003', ...
@@ -16,23 +17,24 @@ if ~exist(trainingSetDir, 'dir'); mkdir(trainingSetDir); end
 
 if ~exist([trainingSetDir 'trainingData.mat'], 'file')
     trainingData = createTrainingDataStruct(sessions, frameNum, obsPortion);
-    save([trainingSetDir 'trainingData.mat'], 'trainingData')
+    save([trainingSetDir 'trainingData.mat'], 'trainingData', 'view')
 else
     fprintf('%s already exists... did not create file\n', trainingSetName);
 end
 
 %% label things
 
-trainingSetName = 'newTrainingData';
+trainingSetName = 'topBotCat';
 
 trainingSetDir = [getenv('OBSDATADIR') 'tracking\trainingData\deepLabCut\' trainingSetName '\'];
-features = {'pawTL', 'pawTR', 'pawBR', 'pawBL', 'gen', 'tailBase', 'tailMid', 'tailEnd'};
+% features = {'pawTL', 'pawTR', 'pawBR', 'pawBL', 'gen', 'tailBase', 'tailMid', 'tailEnd'};
+features = {'paw1', 'paw2', 'paw3', 'paw4', 'gen', 'tailBase', 'tailMid', 'tailEnd', 'paw1LH', 'paw2LF', 'paw3RF', 'paw4RH'}; % with top view
 labelFrames(trainingSetDir, features);
 
 
 
 %% prepare data for deepLabCut
-trainingSetName = 'newTrainingData';
+trainingSetName = 'topBotCat';
 features = {'pawTL', 'pawTR', 'pawBR', 'pawBL','gen', 'tailBase', 'tailMid', 'tailEnd'}; % excluding genitals
 
 trainingSetDir = [getenv('TRAININGEXAMPLESDIR') 'deepLabCut\' trainingSetName '\'];
--- tracking/deepLabCut/labelFrames.m
+++ tracking/deepLabCut/labelFrames.m
@@ -10,7 +10,7 @@ labelledColor = [1 1 0];
 
 % initializations
 defaultPositions = repmat([10 10], length(features), 1) .* repmat(1:length(features),2,1)';
-load([trainingSetDir 'trainingData.mat'], 'trainingData');
+load([trainingSetDir 'trainingData.mat'], 'trainingData', 'view');
 fields = fieldnames(trainingData);
 structInd = 1;
 selectedPoint = nan;
@@ -20,11 +20,28 @@ selectedPoint = nan;
 % create figure
 currentSession = trainingData(structInd).session;
 currentFrame = trainingData(structInd).frameNum;
-vid = VideoReader([getenv('OBSDATADIR') 'sessions\' currentSession '\runBot.mp4']);
+
+if strcmp(view, 'top')
+    vid = VideoReader([getenv('OBSDATADIR') 'sessions\' currentSession '\runTop.mp4']);
+    hgt = vid.Height;
+elseif strcmp(view, 'bot')
+    vid = VideoReader([getenv('OBSDATADIR') 'sessions\' currentSession '\runBot.mp4']);
+    hgt = vid.Height;
+elseif strcmp(view, 'both')
+    vid = VideoReader([getenv('OBSDATADIR') 'sessions\' currentSession '\runTop.mp4']);
+    vidBot = VideoReader([getenv('OBSDATADIR') 'sessions\' currentSession '\runBot.mp4']);
+    hgt = vid.Height + vidBot.Height;
+end
+
 fig = figure('name', sprintf('%s, frame %i', currentSession, currentFrame), ...
-    'menubar', 'none', 'color', 'white', 'keypressfcn', @keypress, 'position', [200 200 vid.Width*vidScaling, vid.Height*vidScaling]);
+    'menubar', 'none', 'color', 'white', 'keypressfcn', @keypress, 'position', [200 200 vid.Width*vidScaling, hgt*vidScaling]);
 colormap gray
-im = image(rgb2gray(read(vid, currentFrame)), 'CDataMapping', 'scaled'); hold on
+if strcmp(view, 'both')
+    imgCat = cat(1, rgb2gray(read(vid, currentFrame)), rgb2gray(read(vidBot, currentFrame)));
+    im = image(imgCat, 'CDataMapping', 'scaled'); hold on
+else
+    im = image(rgb2gray(read(vid, currentFrame)), 'CDataMapping', 'scaled'); hold on
+end
 set(gca, 'position', [0 0 1 1], 'visible', 'off')
 selectedCirc = scatter(0, 0, 200, [.5 .5 1], 'linewidth', 3, 'visible', 'off'); hold on
 
@@ -114,7 +131,7 @@ function keypress(~,~)
                 
             % s: save progress
             case 115
-                save([trainingSetDir 'trainingData.mat'], 'trainingData');
+                save([trainingSetDir 'trainingData.mat'], 'trainingData', 'view');
                 m = msgbox('saving progress...'); pause(.5); close(m)
         end
     end
@@ -135,11 +152,27 @@ function updateFrame(frameStep)
     if ~strcmp(currentSession, trainingData(structInd).session)
         currentSession = trainingData(structInd).session;
         m = msgbox(sprintf('loading session %s...', currentSession)); pause(.5); close(m);
-        vid = VideoReader([getenv('OBSDATADIR') 'sessions\' currentSession '\runBot.mp4']);
+        
+        if strcmp(view, 'top')
+            vid = VideoReader([getenv('OBSDATADIR') 'sessions\' currentSession '\runTop.mp4']);
+            hgt = vid.Height;
+        elseif strcmp(view, 'bot')
+            vid = VideoReader([getenv('OBSDATADIR') 'sessions\' currentSession '\runBot.mp4']);
+            hgt = vid.Height;
+        elseif strcmp(view, 'both')
+            vid = VideoReader([getenv('OBSDATADIR') 'sessions\' currentSession '\runTop.mp4']);
+            vidBot = VideoReader([getenv('OBSDATADIR') 'sessions\' currentSession '\runBot.mp4']);
+            hgt = vid.Height + vidBot.Height;
+        end
+
     end
     
     % update frame
-    frame = rgb2gray(read(vid, trainingData(structInd).frameNum));
+    if strcmp(view, 'both')
+        frame = cat(1, rgb2gray(read(vid, currentFrame)), rgb2gray(read(vidBot, currentFrame)));
+    else
+        frame = rgb2gray(read(vid, trainingData(structInd).frameNum));
+    end
     set(im, 'CData', frame);
     if trainingData(structInd).includeFrame; includedString = '(included)'; else; includedString = ''; end
     set(fig, 'name', sprintf('%s, frame %i %s', currentSession, currentFrame, includedString))
@@ -182,7 +215,7 @@ end
 function constrainedPos = constrainPosition(newPos)
     constrainedPos(1) = min(newPos(1), vid.Width);
     constrainedPos(1) = max(constrainedPos(1), 1);
-    constrainedPos(2) = min(newPos(2), vid.Height);
+    constrainedPos(2) = min(newPos(2), hgt);
     constrainedPos(2) = max(constrainedPos(2), 1); 
 end
 
@@ -197,7 +230,7 @@ function constrainedPos = tailEndConstrainPosition(newPos)
     elseif newPos(2)>vid.Height
         constrainedPos(1) = min(newPos(1), vid.Width);
         constrainedPos(1) = max(constrainedPos(1), 1);
-        constrainedPos(2) = vid.Height;
+        constrainedPos(2) = hgt;
     else
         constrainedPos(1) = 1;
         constrainedPos(2) = newPos(2);
