353c42d076036600e30601d7f9fa0284338fe59f
rwarren2163 <richardwarren2163@gmail.com>
2018-11-03 12:25:16 -0400

    wip

---
+++ ephys/probeMappings/mapGenerators.m
@@ -0,0 +1,100 @@
+%% GENERATE MAPS FOR KILOSORT (I THINK THIS IS FOR VISUALIZATION PURPOSES ONLY)
+
+
+
+
+%% NeuronexusA4x16-Poly2, intan
+
+kiloSortInds = [6 8 10 12 14 16 4 2 1 3 15 13 11 9 7 5 22 24 26 28 30 32 20 18 17 19 31 29 27 25 23 21 38 40 42 44 46 48 36 34 33 35 47 45 43 41 39 37 54 56 58 60 62 64 52 50 49 51 63 61 59 57 55 53];
+intanInds = [47 43 42 39 38 37 45 34 41 32 36 49 35 51 33 53 48 55 50 57 52 60 54 62 56 58 63 61 59 44 46 40 22 16 18 5 3 1 4 6 0 8 2 10 7 12 9 14 11 31 13 29 15 26 30 23 28 19 27 24 25 20 21 17] + 1;
+
+% get desired site locations map
+shankSeparation = 100;
+siteLocations = nan(16, 2); % start with one shank, then replicate after
+siteLocations(2:2:16,:) = cat(2, zeros(8,1), (0:46:46*7)'+23); % first column of leftmost shank
+siteLocations(1:2:15,:) = cat(2, ones(8,1)*30, (0:46:46*7)'); % second column of leftmost shank
+
+siteLocations = repmat(siteLocations,4,1);
+siteLocations(:,1) = siteLocations(:,1) + repelem(1:4,16)'.*shankSeparation;
+
+
+% visualize final mapping results
+close all; figure;
+scatter(siteLocations(:,1), siteLocations(:,2))
+for i = 1:size(siteLocations,1)
+    text(siteLocations(i,1), siteLocations(i,2), num2str(i))
+end
+
+% apply channel mapping to site locations
+siteLocationsRemapped = nan(size(siteLocations));
+for i = 1:64
+    shankLocation = siteLocations(kiloSortInds(i),:);
+    siteLocationsRemapped(intanInds(i),:) = shankLocation;
+end
+daspect([1 1 1])
+
+
+Nchannels = 64;
+connected = true(Nchannels, 1);
+chanMap   = 1:Nchannels;
+chanMap0ind = chanMap - 1;
+xcoords   = siteLocationsRemapped(:,1);
+ycoords   = siteLocationsRemapped(:,2);
+kcoords   = ones(Nchannels,1); % grouping of channels (i.e. tetrode groups)
+
+fs = 30000; % sampling frequency
+save('Y:\obstacleData\ephys\channelMaps\kilosort\mapNeuronexusA4x16-Poly2Intan.mat', ...
+    'chanMap','connected', 'xcoords', 'ycoords', 'kcoords', 'chanMap0ind', 'fs')
+
+%% NeuronexusA1x32Poly3, intan
+
+neuro = [23 25 27 29 31 19 17 21 11 15 13 1 3 5 7 9;
+         24 26 28 30 32 20 18 22 12 16 14 2 4 6 8 10];
+
+intan = [23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8;
+         24 25 26 27 28 29 30 31 0 1 2 3 4 5 6 7];
+intan = intan+1;
+intan = fliplr(intan);
+
+
+connectorInds = [16 6 5 15 4 7 3 8 2 9 1 10 14 13 12 11 22 21 20 19 23 25 24 18 26 17 27 29 28 31 30 32];
+kiloSortInds = [31 28 25 22 19 16 13 10 7 4 32 26 20 14 8 2 1 5 11 17 23 29 3 6 9 12 15 18 21 24 27 30];
+intanInds = intan(arrayfun(@(x) find(neuro==x), connectorInds));
+
+% get desired site locations map
+siteLocations = nan(32, 2);
+siteLocations(4:3:31, :) = cat(2, ones(10,1)*-18, fliplr(12.5 : 25 : (12.5+25*9))'); % xy values for left column
+siteLocations([1 2:3:32], :) = cat(2, zeros(12,1), fliplr(0 : 25 : (25*11))'); % xy values for right column
+siteLocations(3:3:30, :) = cat(2, ones(10,1)*18, fliplr(12.5 : 25 : (12.5+25*9))'); % xy values for right column
+
+% visualize final mapping results
+close all; figure;
+scatter(siteLocations(:,1), siteLocations(:,2))
+for i = 1:size(siteLocations,1)
+    text(siteLocations(i,1), siteLocations(i,2), num2str(i))
+end
+
+% apply channel mapping to site locations
+siteLocationsRemapped = nan(size(siteLocations));
+for i = 1:32
+    shankLocation = siteLocations(kiloSortInds(i),:);
+    siteLocationsRemapped(intanInds(i),:) = shankLocation;
+end
+
+
+
+Nchannels = 32;
+connected = true(Nchannels, 1);
+chanMap   = 1:Nchannels;
+chanMap0ind = chanMap - 1;
+xcoords   = siteLocationsRemapped(:,1);
+ycoords   = siteLocationsRemapped(:,2);
+kcoords   = ones(Nchannels,1); % grouping of channels (i.e. tetrode groups)
+
+fs = 30000; % sampling frequency
+save('Y:\obstacleData\ephys\channelMaps\kilosort\mapNeuronexusA1x32Poly3Intan.mat', ...
+    'chanMap','connected', 'xcoords', 'ycoords', 'kcoords', 'chanMap0ind', 'fs')
+
+
+
+
--- ephys/showChannelsOverTime.m
+++ ephys/showChannelsOverTime.m
@@ -1,21 +1,23 @@
 
 
 % settings
-channels = 1:32; % 4
+channels = 1:2:64; % 4
 highPass = 300;
 fs = 30000;
-folder = 'Y:\obstacleData\sessions\180917_002\2018-09-17_15-46-56';
+folder = 'Y:\obstacleData\sessions\181030_000\ephys_2018-10-30_18-27-21';
 yLims = [-500 500];
 windowSize = .5;
 timeIntervals = 5*60;
 
 % mapping
-channelIn = [31 27 22 18 28 23 21 26 29 24 20 25 30 19 32 17 1 16 3 14 9 10 8 2 7 15 11 12 6 13 5 4];
-channelOut = [31 28 25 22 19 16 13 10 7 4 32 26 20 14 8 2 1 5 11 17 23 29 3 6 9 12 15 18 21 24 27 30];
+% channelIn = [31 27 22 18 28 23 21 26 29 24 20 25 30 19 32 17 1 16 3 14 9 10 8 2 7 15 11 12 6 13 5 4];
+% channelOut = [31 28 25 22 19 16 13 10 7 4 32 26 20 14 8 2 1 5 11 17 23 29 3 6 9 12 15 18 21 24 27 30];
+channelIn = 1:64;
+channelOut = 1:64;
 
 % initializations
 addpath(fullfile(getenv('GITDIR'), 'analysis-tools'))
-[~, times] = load_open_ephys_data_faster(fullfile(folder, '100_CH1.continuous'));
+[~, times] = load_open_ephys_data_faster(fullfile(folder, '107_CH1.continuous'));
 timeStarts = min(times):timeIntervals:max(times);
 
 figure('color', 'white', 'Units', 'normalized', 'Position', [0 0 1 1]);
@@ -24,7 +26,7 @@ for i = 1:length(channels)
     disp(i)
     chan = channelOut(find(channelIn==i,1,'first'));
     
-    [data, times, info] = load_open_ephys_data_faster(fullfile(folder, ['100_CH' num2str(channels(i)) '.continuous']));
+    [data, times, info] = load_open_ephys_data_faster(fullfile(folder, ['107_CH' num2str(channels(i)) '.continuous']));
     dataHpf = highpass(data, highPass, fs);
     
     for j = 1:length(timeStarts)
