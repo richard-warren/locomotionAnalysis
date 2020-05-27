ec6a70ffa0925b450ab05d03e256608ca7c01c59
rwarren2163 <richardwarren2163@gmail.com>
2020-05-27 15:19:02 -0400

    wip

--- tracking/showTracking.m
+++ tracking/showTracking.m
@@ -3,20 +3,22 @@ function showTracking(session, varargin)
 % to do: show confidence and gobal class in bottom left // think about how
 % i should handle confidence thresholds...
 
-% additional features: paw touches // arbitrary signal // arbitrary times
-% // wheel // post-processed tracking // whisker overlay // whisker
-% contacts // confidences // go to water, whisker contact
+% todo: whisker angle // arbitrary signal // arbitrary times? // whisker contacts // import skeleton?
+% events: whisker contact // paw contact // licks // spike times
 
 % settings
+s.includeWiskCam = true;  % whether to add whisker camera
 s.scoreThresh = .8;
-vidFs = 250;
-vidDelay = .02;
-showDlcScores = false;
-showTouchData = true;
-showStance = true;
-circSize = 100;
-vidSizeScaling = 1.5;
-colorMap = 'hsv';
+s.vidDelay = .02;
+s.showConfidence = false;
+s.showPawTouch = true;
+s.showPawTouchConfidence = true;
+s.showStance = true;
+s.circSize = 100;
+s.vidScaling = 1.5;
+s.colorMap = 'hsv';
+s.faceColor = 'yellow';  % color for face tracking points
+s.frameProps = {'isPaddingWhite', false, 'edgeFading', 5, 'border', 2};  % arguments to be passed to getFrameWithWisk (only when includeWiskCam=true)
 connectedFeatures = {{'paw1LH_bot', 'paw1LH_top'}, ...
                      {'paw2LF_bot', 'paw2LF_top'}, ...
                      {'paw3RF_bot', 'paw3RF_top'}, ...
@@ -26,44 +28,85 @@ connectedFeatures = {{'paw1LH_bot', 'paw1LH_top'}, ...
                      {'obsHigh_bot', 'obsLow_bot'}}; % features that are connected within a view (not across views)
 
 
-
+% initializations
 if exist('varargin', 'var'); for i = 1:2:length(varargin); s.(varargin{i}) = varargin{i+1}; end; end  % parse name-value pairs
 
 % load video
-vidName = fullfile(getenv('OBSDATADIR'), 'sessions', session, 'run.mp4');
-if ~exist(vidName, 'file'); concatTopBotVids(session); end  % old sessions were recorded with separate top and bot views, which need to be concatenated
-vid = VideoReader(vidName);
+vid = VideoReader(fullfile(getenv('OBSDATADIR'), 'sessions', session, 'run.mp4'));
+if s.includeWiskCam; vidWisk = VideoReader(fullfile(getenv('OBSDATADIR'), 'sessions', session, 'runWisk.mp4')); end
 
 % get locations data and convert to 3d matrix
 load(fullfile(getenv('OBSDATADIR'), 'sessions', session, 'runAnalyzed.mat'), ...
-    'frameTimeStamps', 'wheelPositions', 'wheelTimes', 'pixelsPerM', ...
-    'wheelCenter', 'wheelRadius', 'touchesPerPaw', 'touchClassNames', 'touchConfidences', 'obsOnTimes', 'isLightOn');
+    'frameTimeStamps', 'frameTimeStampsWisk', 'wheelPositions', 'wheelTimes', 'pixelsPerM', 'wiskContactTimes', 'rewardTimes', ...
+    'wheelCenter', 'wheelRadius', 'touchesPerPaw', 'touchClassNames', 'touchConfidences', 'obsOnTimes', 'isLightOn', 'whiskerAngle');
 locationsTable = readtable(fullfile(getenv('OBSDATADIR'), 'sessions', session, 'trackedFeaturesRaw.csv')); % get raw tracking data
 [locations, features, ~, isInterped, scores] = fixTracking(locationsTable, frameTimeStamps, pixelsPerM, 'scoreThresh', s.scoreThresh);
 topPawInds = find(contains(features, 'paw') & contains(features, '_top'));
 botPawInds = find(contains(features, 'paw') & contains(features, '_bot'));
-if showStance
+if s.showStance
     stanceBins = getStanceBins(frameTimeStamps, locations(:,:,topPawInds), wheelPositions, ...
-        wheelTimes, wheelCenter, wheelRadius, vidFs, pixelsPerM);
+        wheelTimes, wheelCenter, wheelRadius, 1/median(diff(frameTimeStamps)), pixelsPerM);
 end
 
 
+% get position where wisk frame should overlap with runTop frame
+if s.includeWiskCam
+    [frame, yWiskPos, xWiskPos, wiskScaling] = ...
+        getFrameWithWisk(vid, vidWisk, frameTimeStamps, frameTimeStampsWisk, find(frameTimeStamps>obsOnTimes(1), 1, 'first'), s.frameProps{:});  % use first frame where obstacle is on to ensure mouse is on the wheel when the whisker cam position is determined
+else
+    frame = read(vid, 1);
+end
+sz = [size(frame,1) size(frame,2) 3];
+
+
 % set up figure
-figureName = session;
-hgt = vid.Height;
-fig = figure('name', figureName, 'units', 'pixels', 'position', [600 100 vid.Width*vidSizeScaling hgt*vidSizeScaling],...
+fig = figure('name', session, 'units', 'pixels', 'position', [600 100 sz(1)*s.vidScaling sz(2)*s.vidScaling],...
     'menubar', 'none', 'color', 'black', 'keypressfcn', @changeFrames);
 colormap gray
-imPreview = image(zeros(hgt, vid.Width), 'CDataMapping', 'scaled'); hold on;
+imPreview = image(frame, 'CDataMapping', 'scaled'); hold on;
 imAxis = gca;
-set(imAxis, 'visible', 'off', 'units', 'pixels',...
-    'position', [0 0 vid.Width*vidSizeScaling hgt*vidSizeScaling]);
+set(imAxis, 'visible', 'off', 'units', 'pixels', 'units', 'normalized', 'position', [0 0 1 1]);
+
 
 % draw circle at wheel location
 viscircles(wheelCenter', wheelRadius, 'color', 'blue');
 
+
+% add whisker features
+if s.includeWiskCam
+    wiskFeatures = {'jaw', 'tongue', 'nose'};
+    
+    % remove locations from run tracking that will be in wisk tracking
+    locations(:,:,contains(features, wiskFeatures)) = nan;
+    
+    % load whisker tracking
+    locationsWiskTable = readtable(fullfile(getenv('OBSDATADIR'), 'sessions', session, 'trackedFeaturesRaw_wisk.csv')); % get raw tracking data
+    locationsWisk = nan(height(locationsWiskTable), 2, length(wiskFeatures));
+    
+    % find whisker pad and length
+    pad = [median(locationsWiskTable.wisk_pad) median(locationsWiskTable.wisk_pad_1)];  % location of whisker pad
+    pad = pad*wiskScaling + [xWiskPos yWiskPos];
+    tipAvg = [median(locationsWiskTable.wisk_caudal) median(locationsWiskTable.wisk_caudal_1)]*wiskScaling + [xWiskPos yWiskPos];
+    wiskLength = norm(tipAvg - pad);
+    
+    % fix tracking
+    for i = 1:length(wiskFeatures)
+        valid = locationsWiskTable.([wiskFeatures{i} '_2']) > s.scoreThresh;
+        
+        locationsWisk(valid,1,i) = locationsWiskTable.(wiskFeatures{i})(valid)*wiskScaling + xWiskPos;
+        locationsWisk(valid,2,i) = locationsWiskTable.([wiskFeatures{i} '_1'])(valid)*wiskScaling + yWiskPos;
+    end
+    
+    clear locationsWiskTable
+    scatterWisk = scatter(imAxis, zeros(1,length(wiskFeatures)), zeros(1,length(wiskFeatures)), 20, s.faceColor, 'filled');
+    
+    % create whisker angle line
+    wisk = plot([0 0], [0 0], 'color', s.faceColor, 'LineWidth', 2);
+end
+
+
 % set colors s.t. matching features in top and bot view have same color
-cmap = eval(sprintf('%s(%i);', colorMap, length(features)));
+cmap = eval(sprintf('%s(%i);', s.colorMap, length(features)));
 
 
 % set up lines joining features within a view
@@ -80,28 +123,34 @@ end
 
 % set up scatter points for tracked features
 scatterLocations = scatter(imAxis, zeros(1,length(features)), zeros(1,length(features)),...
-    circSize, cmap, 'linewidth', 3); hold on
+    s.circSize, cmap, 'linewidth', 3); hold on
+
 
 % set up scatter points that will surround paw when it is touching obs
-obsTouchScatter = scatter(imAxis, [], [], circSize*3, [1 1 1], 'LineWidth', 2);
+if s.showPawTouch
+    obsTouchScatter = scatter(imAxis, [], [], s.circSize*3, [1 1 1], 'LineWidth', 2);
+end
+
 
 % set up stance scatter points
-if showStance
+if s.showStance
     scatterStance = scatter(imAxis, ...
         zeros(1,length([botPawInds topPawInds])), zeros(1,length([botPawInds topPawInds])), ...
-        circSize, cmap([botPawInds topPawInds],:), 'filled'); hold on
+        s.circSize, cmap([botPawInds topPawInds],:), 'filled'); hold on
 end
 
-% set up text to show dlc scores
-scoreLabels = cell(1,length(features));
-if showDlcScores
-    for i = 1:length(features); scoreLabels{i} = text(0,0,'', 'color', cmap(i,:)); end
+
+% set up text to show confidence
+if s.showConfidence
+    confidenceLabels = cell(1,length(features));
+    for i = 1:length(features); confidenceLabels{i} = text(0,0,'', 'color', cmap(i,:)); end
 end
+
 
 % set up text to show touch tracking info
-touchScoreLabels = cell(1,length(features));
-if showTouchData
-    for i = 1:4; touchScoreLabels{i} = text(0,0,'', 'color', [1 1 1], 'interpreter', 'none'); end
+if s.showPawTouchConfidence
+    touchConfidenceLabels = cell(1,length(features));
+    for i = 1:4; touchConfidenceLabels{i} = text(0,0,'', 'color', [1 1 1], 'interpreter', 'none'); end
 end
 
 % set state variables
@@ -156,6 +205,18 @@ function changeFrames(~,~)
             input = inputdlg('enter trial number');
             frameInd = find(frameTimeStamps>=obsOnTimes(str2num(input{1})),1,'first');
             updateFrame(0);
+        
+        % 'w': go to next water drop
+        elseif key==119
+            nextRewardTime = rewardTimes(find(rewardTimes>frameTimeStamps(frameInd), 1, 'first'));
+            frameInd = find(frameTimeStamps>nextRewardTime, 1, 'first');
+            updateFrame(0);
+        
+        % 'o': go to next whisker contact
+        elseif key==111
+            nextContactTime = wiskContactTimes(find(wiskContactTimes>frameTimeStamps(frameInd), 1, 'first'));
+            frameInd = find(frameTimeStamps>nextContactTime, 1, 'first');
+            updateFrame(0);
             
         % ESCAPE: close window
         elseif key==27                  
@@ -178,11 +239,16 @@ function updateFrame(frameStep)
     if frameInd < 1; frameInd = vid.NumberOfFrames;
     elseif frameInd > vid.NumberOfFrames; frameInd = 1; end
     
-    
     % get frame and sub-frames
-    frame = rgb2gray(read(vid, frameInd));
+    if s.includeWiskCam
+        frame = getFrameWithWisk(vid, vidWisk, frameTimeStamps, frameTimeStampsWisk, frameInd, ...
+            'yWiskPos', yWiskPos, 'xWiskPos', xWiskPos, 'wiskScaling', wiskScaling, s.frameProps{:});
+        frame = repmat(frame, 1, 1, 3);  % add color dimension
+    else
+        frame = read(vid, frameInd);
+    end
     
-	% add frame number
+	% add metadata
     trial = find(obsOnTimes>=frameTimeStamps(frameInd),1,'first')-1;
     if trial
         if isLightOn(trial); lightText = 'light on'; else; lightText = 'light off'; end
@@ -190,7 +256,7 @@ function updateFrame(frameStep)
         lightText = '';
     end
     frame = insertText(frame, [size(frame,2) size(frame,1)], ...
-        sprintf('frame %i, trial %i, %s', ...
+        sprintf('frame %i trial %i %s', ...
         frameInd, trial, lightText), ...
         'BoxColor', 'black', 'AnchorPoint', 'RightBottom', 'TextColor', 'white');
     
@@ -207,11 +273,11 @@ function updateFrame(frameStep)
     % upate scatter positions
     set(scatterLocations, 'XData', locations(frameInd,1,:), ...
         'YData', locations(frameInd,2,:), ...
-        'SizeData', ones(1,length(features))*circSize - (ones(1,length(features)) ...
-                    .* isInterped(frameInd,:)) * circSize * .9);
+        'SizeData', ones(1,length(features))*s.circSize - (ones(1,length(features)) ...
+                    .* isInterped(frameInd,:)) * s.circSize * .9);
     
     % update scatter stance positions
-    if showStance
+    if s.showStance
         isStance = repmat(stanceBins(frameInd,:),1,2);
         set(scatterStance, ...
             'XData', squeeze(locations(frameInd,1,[botPawInds topPawInds])) .* isStance', ...
@@ -219,7 +285,7 @@ function updateFrame(frameStep)
     end
     
     % update paw touch scatter
-    if exist('touchesPerPaw', 'var')
+    if s.showPawTouch
         touchingBins = touchesPerPaw(frameInd,:)>0;
         x = locations(frameInd, 1, [topPawInds(touchingBins) botPawInds(touchingBins)]);
         y = locations(frameInd, 2, [topPawInds(touchingBins) botPawInds(touchingBins)]);
@@ -227,28 +293,39 @@ function updateFrame(frameStep)
     end
 
     % update scores text
-    if showDlcScores
+    if s.showConfidence
         for j = 1:length(features)
-            set(scoreLabels{j}, 'position', [locations(frameInd,1,j)+10, locations(frameInd,2,j)], ...
+            set(confidenceLabels{j}, 'position', [locations(frameInd,1,j)+10, locations(frameInd,2,j)], ...
                 'string', sprintf('%.2f', scores(frameInd,j)));
         end
     end
     
-    if showTouchData
+    % update paw touch confidence text
+    if s.showPawTouchConfidence
         for j = 1:4
             classInd = touchesPerPaw(frameInd,j);
             if classInd==0; classInd=find(strcmp(touchClassNames, 'no_touch')); end
             class = touchClassNames{classInd};
             confidence = touchConfidences(frameInd);
-            set(touchScoreLabels{j}, ...
+            set(touchConfidenceLabels{j}, ...
                 'position', [locations(frameInd,1,topPawInds(j))+10, locations(frameInd,2,topPawInds(j))], ...
                 'string', sprintf('%s (%.2f)', class, confidence));
             
         end
     end
+    
+    % update whisker view tracking
+    if s.includeWiskCam
+        set(scatterWisk, 'XData', locationsWisk(frameInd,1,:), 'YData', locationsWisk(frameInd,2,:));
+        
+        % whisker angle
+        x = cosd(whiskerAngle(frameInd)) * wiskLength;
+        y = -sind(whiskerAngle(frameInd)) * wiskLength;
+        set(wisk, 'XData', [pad(1) pad(1)+x], 'YData', [pad(2) pad(2)+y])
+    end
 
     % pause to reflcet on the little things...
-    pause(vidDelay);
+    pause(s.vidDelay);
 end
 
 
