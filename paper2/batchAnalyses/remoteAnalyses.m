e3c3cb0cc65ba2b616db35a65eadec98ec1c5ca3
rwarren2163 <richardwarren2163@gmail.com>
2020-11-23 12:11:30 -0500

    adding getExperimentData to remote analyses

--- paper2/batchAnalyses/remoteAnalyses.m
+++ paper2/batchAnalyses/remoteAnalyses.m
@@ -1,8 +1,6 @@
 %% perform analyses that require access to engram, which is slow from home ethernet
 
 sessions = getEphysSessions();
-% sessions = sessions(1:33);  % temp
-sessions = {'181020_001'};
 overwrite = true;
 dt = .01;
 
@@ -10,15 +8,21 @@ tic
 parfor i = 1:length(sessions)
     try
         % format ephys data
-        filename = fullfile(getenv('OBSDATADIR'), 'data_transfer', 'neuralData', [sessions{i} '_neuralData.mat']);
-        if overwrite || ~exist(filename, 'file')
-            formatEphysData(sessions{i}, 'outputFileName', filename, 'kernel', 'gauss', 'kernelSig', .02, 'plot', false)
-        end
+%         filename = fullfile(getenv('OBSDATADIR'), 'data_transfer', 'neuralData', [sessions{i} '_neuralData.mat']);
+%         if overwrite || ~exist(filename, 'file')
+%             formatEphysData(sessions{i}, 'outputFileName', filename, 'kernel', 'gauss', 'kernelSig', .02, 'plot', false)
+%         end
         
         % predictors
-        filename = fullfile(getenv('OBSDATADIR'), 'data_transfer', 'predictors', [sessions{i} '_predictors.mat']);
+%         filename = fullfile(getenv('OBSDATADIR'), 'data_transfer', 'predictors', [sessions{i} '_predictors.mat']);
+%         if overwrite || ~exist(filename, 'file')
+%             getPredictors(sessions{i}, 'outputFileName', filename, 'plot', true, 'visible', 'off', 'dt', dt)
+%         end
+        
+        % experimentData (for single session)
+        filename = fullfile(getenv('OBSDATADIR'), 'data_transfer', 'expData', [sessions{i} '_expData.mat']);
         if overwrite || ~exist(filename, 'file')
-            getPredictors(sessions{i}, 'outputFileName', filename, 'plot', true, 'visible', 'off', 'dt', dt)
+            getExperimentData(sessions{i}, 'all', 'outputFileName', filename);
         end
             
     catch exception
